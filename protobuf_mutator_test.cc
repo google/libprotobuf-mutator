cddc71414988481c6b669331676afb4c029e73d3
Vitaly Buka <vitalybuka@chromium.org>
2016-11-16 15:40:15 -0800

    Main is not needed.
    Change-Id: I1c1899492d4b3b073675565c3747f7cf132d7d9b

--- protobuf_mutator_test.cc
+++ protobuf_mutator_test.cc
@@ -21,8 +21,3 @@ TEST(ProtobufMutator, Empty) {
   tutorial::AddressBook message;
   EXPECT_TRUE(1);
 }
-
-// int main(int argc, char **argv) {
-//   ::testing::InitGoogleTest(&argc, argv);
-//   return RUN_ALL_TESTS();
-// }
