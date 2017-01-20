8da52815c1ea19435da656f68c0472f15a0ee885
Vitaly Buka <vitalybuka@chromium.org>
2017-01-20 15:14:44 -0800

    Move examples/libfuzzer* to examples/libfuzzer/libfuzzer_example*

--- examples/libfuzzer/libfuzzer_example.cc
+++ examples/libfuzzer/libfuzzer_example.cc
@@ -15,7 +15,7 @@
 #include <cstddef>
 #include <cstdint>
 
-#include "libfuzzer.pb.h"  // NOLINT
+#include "libfuzzer_example.pb.h"  // NOLINT
 #include "src/libfuzzer_protobuf_mutator.h"
 
 using libfuzzer_example::Msg;
