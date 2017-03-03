4e4243c779c6a68b4daff5333e012bca1e057c82
Vitaly Buka <vitalybuka@google.com>
2017-03-03 14:32:47 -0800

    Add ConstFieldInstance::name(). Helpful when debugging.

--- src/field_instance.h
+++ src/field_instance.h
@@ -164,6 +164,8 @@ class ConstFieldInstance {
     (*value)->CopyFrom(source);
   }
 
+  const std::string& name() const { return descriptor_->name(); }
+
   protobuf::FieldDescriptor::CppType cpp_type() const {
     return descriptor_->cpp_type();
   }
