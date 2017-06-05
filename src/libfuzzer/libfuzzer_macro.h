df7e08cc47eebc4ceded835a5894726fd6be3017
Vitaly Buka <vitalybuka@google.com>
2017-06-05 16:09:50 -0700

    Cleanup DEFINE_PROTO_FUZZER macro and implementation.

--- src/libfuzzer/libfuzzer_macro.h
+++ src/libfuzzer/libfuzzer_macro.h
@@ -17,69 +17,58 @@
 
 #include <stddef.h>
 #include <cstdint>
+#include <type_traits>
 
 #include "port/protobuf.h"
 
-// Defines custom mutator, crossover and test function.
-#define DEFINE_PROTO_FUZZER(Proto) DEFINE_TEXT_PROTO_FUZZER(Proto)
+// Defines custom mutator, crossover and test functions using default
+// serialization format. Default is text.
+#define DEFINE_PROTO_FUZZER(arg) DEFINE_TEXT_PROTO_FUZZER(arg)
+// Defines custom mutator, crossover and test functions using text
+// serialization. This format is more convenient to read.
+#define DEFINE_TEXT_PROTO_FUZZER(arg) DEFINE_PROTO_FUZZER_IMPL(false, arg)
+// Defines custom mutator, crossover and test functions using binary
+// serialization. This makes mutations faster. However often test function is
+// significantly slower than mutator, so fuzzing rate may stay unchanged.
+#define DEFINE_BINARY_PROTO_FUZZER(arg) DEFINE_PROTO_FUZZER_IMPL(true, arg)
 
-// Defines custom mutator, crossover and test function for messages serialized
-// as binary.
-#define DEFINE_BINARY_PROTO_FUZZER(Proto) DEFINE_PROTO_FUZZER_PARAM(true, Proto)
-
-// Defines custom mutator, crossover and test function for messages serialized
-// as text.
-#define DEFINE_TEXT_PROTO_FUZZER(Proto) DEFINE_PROTO_FUZZER_PARAM(false, Proto)
-
-// Usage: define the following alongside your LLVMFuzzerTestOneInputwhich to
-//        parse input with proto2::TextFormat::ParseFromString.
-//  DEFINE_CUSTOM_PROTO_MUTATOR(YourMessageType)
-#define DEFINE_CUSTOM_PROTO_MUTATOR(Proto) \
-  DEFINE_CUSTOM_PROTO_MUTATOR_PARAM(false, Proto)
-
-// Usage: define the following alongside your LLVMFuzzerTestOneInput to parse
-//        input with proto2::Message::ParseFromString.
-//  DEFINE_CUSTOM_PROTO_MUTATOR_PARAM(YourMessageType)
-#define DEFINE_CUSTOM_BINARY_PROTO_MUTATOR(Proto) \
-  DEFINE_CUSTOM_PROTO_MUTATOR_PARAM(true, Proto)
-
-// Usage: Implementation of DEFINE_CUSTOM_PROTO_MUTATOR and
-// DEFINE_CUSTOM_BINARY_PROTO_MUTATOR.
-// |use_binary| if true, mutator will parse and serialize proto as binary, if
-// it false human-readable text format will be used.
-#define DEFINE_CUSTOM_PROTO_MUTATOR_PARAM(use_binary, Proto)                   \
+// Implementation of macros above.
+#define DEFINE_CUSTOM_PROTO_MUTATOR_IMPL(use_binary, Proto)                    \
   extern "C" size_t LLVMFuzzerCustomMutator(                                   \
       uint8_t* data, size_t size, size_t max_size, unsigned int seed) {        \
     using protobuf_mutator::libfuzzer::internal::CustomProtoMutator;           \
     Proto input;                                                               \
     return CustomProtoMutator(use_binary, data, size, max_size, seed, &input); \
-  }                                                                            \
-  extern "C" size_t LLVMFuzzerCustomCrossOver(                                 \
-      const uint8_t* data1, size_t size1, const uint8_t* data2, size_t size2,  \
-      uint8_t* out, size_t max_out_size, unsigned int seed) {                  \
-    using protobuf_mutator::libfuzzer::internal::CustomProtoCrossOver;         \
-    Proto input1;                                                              \
-    Proto input2;                                                              \
-    return CustomProtoCrossOver(use_binary, data1, size1, data2, size2, out,   \
-                                max_out_size, seed, &input1, &input2);         \
   }
 
-// Usage: Implementation of DEFINE_PROTO_FUZZER and DEFINE_BINARY_PROTO_FUZZER.
-// |use_binary| if true, fuzzer will parse and serialize proto as binary, if
-// it false human-readable text format will be used.
-#define DEFINE_PROTO_FUZZER_PARAM(use_binary, arg_decralation)                 \
-  static void TestOneProtoInput(arg_decralation);                              \
+#define DEFINE_CUSTOM_PROTO_CROSSOVER_IMPL(use_binary, Proto)                 \
+  extern "C" size_t LLVMFuzzerCustomCrossOver(                                \
+      const uint8_t* data1, size_t size1, const uint8_t* data2, size_t size2, \
+      uint8_t* out, size_t max_out_size, unsigned int seed) {                 \
+    using protobuf_mutator::libfuzzer::internal::CustomProtoCrossOver;        \
+    Proto input1;                                                             \
+    Proto input2;                                                             \
+    return CustomProtoCrossOver(use_binary, data1, size1, data2, size2, out,  \
+                                max_out_size, seed, &input1, &input2);        \
+  }
+
+#define DEFINE_TEST_ONE_PROTO_INPUT_IMPL(use_binary, Proto)                 \
+  extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) { \
+    using protobuf_mutator::libfuzzer::internal::LoadProtoInput;            \
+    Proto input;                                                            \
+    if (LoadProtoInput(use_binary, data, size, &input))                     \
+      TestOneProtoInput(input);                                             \
+    return 0;                                                               \
+  }
+
+#define DEFINE_PROTO_FUZZER_IMPL(use_binary, arg)                              \
+  static void TestOneProtoInput(arg);                                          \
   using FuzzerProtoType = std::remove_const<std::remove_reference<             \
       std::function<decltype(TestOneProtoInput)>::argument_type>::type>::type; \
-  DEFINE_CUSTOM_PROTO_MUTATOR_PARAM(use_binary, FuzzerProtoType)               \
-  extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {    \
-    using protobuf_mutator::libfuzzer::internal::LoadProtoInput;               \
-    FuzzerProtoType input;                                                     \
-    if (LoadProtoInput(use_binary, data, size, &input))                        \
-      TestOneProtoInput(input);                                                \
-    return 0;                                                                  \
-  }                                                                            \
-  static void TestOneProtoInput(arg_decralation)
+  DEFINE_CUSTOM_PROTO_MUTATOR_IMPL(use_binary, FuzzerProtoType)                \
+  DEFINE_CUSTOM_PROTO_CROSSOVER_IMPL(use_binary, FuzzerProtoType)              \
+  DEFINE_TEST_ONE_PROTO_INPUT_IMPL(use_binary, FuzzerProtoType)                \
+  static void TestOneProtoInput(arg)
 
 namespace protobuf_mutator {
 namespace libfuzzer {
