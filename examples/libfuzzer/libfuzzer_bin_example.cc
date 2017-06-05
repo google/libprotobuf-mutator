794097c507ddc44dbcbf96d6e779255785f8dfbb
Vitaly Buka <vitalybuka@google.com>
2017-06-05 15:25:47 -0700

    Add macros for simple fuzzer implementation.

--- examples/libfuzzer/libfuzzer_bin_example.cc
+++ examples/libfuzzer/libfuzzer_bin_example.cc
@@ -12,42 +12,20 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-#include <cstddef>
-#include <cstdint>
+#include <cmath>
 
 #include "examples/libfuzzer/libfuzzer_example.pb.h"
-#include "src/binary_format.h"
-#include "src/libfuzzer/libfuzzer_mutator.h"
+#include "src/libfuzzer/libfuzzer_macro.h"
 
-using libfuzzer_example::Msg;
 protobuf_mutator::protobuf::LogSilencer log_silincer;
 
-extern "C" size_t LLVMFuzzerCustomMutator(uint8_t* data, size_t size,
-                                          size_t max_size, unsigned int seed) {
-  return protobuf_mutator::libfuzzer::MutateBinaryMessage<Msg>(data, size,
-                                                               max_size, seed);
-}
-
-extern "C" size_t LLVMFuzzerCustomCrossOver(const uint8_t* data1, size_t size1,
-                                            const uint8_t* data2, size_t size2,
-                                            uint8_t* out, size_t max_out_size,
-                                            unsigned int seed) {
-  return protobuf_mutator::libfuzzer::CrossOverBinaryMessages<Msg>(
-      data1, size1, data2, size2, out, max_out_size, seed);
-}
-
-extern "C" int LLVMFuzzerTestOneInput(const uint8_t* data, size_t size) {
-  Msg message;
-  protobuf_mutator::ParseBinaryMessage(data, size, &message);
-
+DEFINE_BINARY_PROTO_FUZZER(const libfuzzer_example::Msg& message) {
   // Emulate a bug.
   if (message.optional_string() == "FooBar" &&
-      message.optional_uint64() == 17 &&
+      message.optional_uint64() > 100 &&
       !std::isnan(message.optional_float()) &&
-      fabs(message.optional_float()) > 1000 &&
-      fabs(message.optional_float()) < 1E10) {
+      std::fabs(message.optional_float()) > 1000 &&
+      std::fabs(message.optional_float()) < 1E10) {
     abort();
   }
-
-  return 0;
 }
