1c562ab6567cb98bf29de03459e36a99971acab6
Vitaly Buka <vitalybuka@google.com>
2018-01-15 14:25:10 -0800

    Update protobuf Disabled leak sanitizer on travis, looks like travis removed SYS_PTRACE.

--- cmake/external/protobuf.cmake
+++ cmake/external/protobuf.cmake
@@ -47,7 +47,7 @@ include (ExternalProject)
 ExternalProject_Add(${PROTOBUF_TARGET}
     PREFIX ${PROTOBUF_TARGET}
     GIT_REPOSITORY https://github.com/google/protobuf.git
-    GIT_TAG 008ff033
+    GIT_TAG 47b7d2c
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ${CMAKE_COMMAND} ${PROTOBUF_INSTALL_DIR}/src/${PROTOBUF_TARGET}/cmake
         -G${CMAKE_GENERATOR}
