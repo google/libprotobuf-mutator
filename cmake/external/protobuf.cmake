aa8ab85b27d8e15a327a1d3a4ff592e8ec3e3ef0
Vitaly Buka <vitalybuka@google.com>
2019-02-04 12:37:48 -0800

    Update protocolbuffers
    Fix google/oss-fuzz#864
    Pickup fix stack overflow in text parser protocolbuffers/protobuf#5661

--- cmake/external/protobuf.cmake
+++ cmake/external/protobuf.cmake
@@ -58,7 +58,7 @@ include (ExternalProject)
 ExternalProject_Add(${PROTOBUF_TARGET}
     PREFIX ${PROTOBUF_TARGET}
     GIT_REPOSITORY https://github.com/google/protobuf.git
-    GIT_TAG 96f7c53
+    GIT_TAG 6dcd810
     UPDATE_COMMAND ""
     CONFIGURE_COMMAND ${CMAKE_COMMAND} ${PROTOBUF_INSTALL_DIR}/src/${PROTOBUF_TARGET}/cmake
         -G${CMAKE_GENERATOR}
