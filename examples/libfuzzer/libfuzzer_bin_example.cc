f12754507c6fa2ff993879d06f77f52678884c8b
Vitaly Buka <vitalybuka@google.com>
2024-01-16 16:18:18 -0800

    Replace DebugString with absl::StrCat
    DebugString is going to be deprecated.

--- examples/libfuzzer/libfuzzer_bin_example.cc
+++ examples/libfuzzer/libfuzzer_bin_example.cc
@@ -55,7 +55,7 @@ DEFINE_BINARY_PROTO_FUZZER(const libfuzzer_example::Msg& message) {
       !std::isnan(message.optional_float()) &&
       std::fabs(message.optional_float()) > 1000 &&
       message.any().UnpackTo(&file) && !file.name().empty()) {
-    std::cerr << message.DebugString() << "\n";
+    std::cerr << absl::StrCat(message) << "\n";
     abort();
   }
 }
